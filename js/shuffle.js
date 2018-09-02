2325c0d7c5c63db301868f143af7af46d1145caf
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2018-09-02 10:25:11 -0700

    hide shuffling until loaded

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     var grid = document.getElementsByClassName("grid")[0];
+    grid.style.visibility = "hidden";
     for (var i = grid.children.length; i >= 0; i--) {
         grid.appendChild(grid.children[Math.random() * i | 0]);
     }
+    grid.style.visibility = "visible";
 });
