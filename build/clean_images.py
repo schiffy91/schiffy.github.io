6283483d56e84e6691bebd6abfcb2a5040d2543a
Alexander Schiffhauer <alexander.schiffhauer@gmail.com>
2025-05-11 20:06:04 -0700

    Removing warnings

--- build/clean_images.py
+++ build/clean_images.py
@@ -2,7 +2,7 @@ import os, sys
 from typing import * #pylint: disable=unused-wildcard-import
 from pathlib import Path
 
-def clean_path(path: Path, ext = ".jpg", remove_files = True, whitelist = [".pth"]):
+def clean_path(path: Path, ext = ".jpg", remove_files = True, allowlist = [".pth"]): #pylint: disable=dangerous-default-value
     for root, _, files in os.walk(path):
         i = 0
         fname_len = len(str(len(files)))
@@ -11,20 +11,19 @@ def clean_path(path: Path, ext = ".jpg", remove_files = True, whitelist = [".pth
             dst = os.path.join(root, str(i).zfill(fname_len) + ext)
             # remove non images
             if remove_files and src[-len(ext):] != ext:
-                if src[-len(ext):] in whitelist:
-                    print("Skipping whitelisted file: " + src)
+                if src[-len(ext):] in allowlist:
+                    print("Skipping allowlisted file: " + src)
                 else:
                     os.remove(src)
-                    print("Removed blacklisted file: " +  src)
-                continue 
+                    print("Removed denylisted file: " +  src)
+                continue
             # check to see if the file is already sorted
-            try: 
+            try:
                 fname_no_ext = fname[:-len(ext)]
                 if len(fname_no_ext) == fname_len and int(fname_no_ext) < len(files):
                     continue
             # an unsorted image
-            except:
-                pass
+            except BaseException: pass
             # prevent renaming images to existing image names
             while os.path.isfile(dst):
                 dst = os.path.join(root, str(i).zfill(fname_len) + ext)
